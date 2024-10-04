1f9c96a66a4fa06ca5a84d79f651986e264f597d
rptl8sr <rptl8sr@gmail.com>
2024-10-05 01:24:59 +0300

    Make app

--- app/app.go
+++ app/app.go
@@ -1,20 +1,44 @@
 package app
 
+import (
+	"log"
+
+	"go-flattern-prices/internal/configs"
+	"go-flattern-prices/internal/controller"
+	"go-flattern-prices/internal/logger"
+	"go-flattern-prices/internal/store"
+)
+
 type app struct {
-	errChan chan error
+	controller controller.Controller
 }
 
 type App interface {
+	Start()
 }
 
 func New() (App, error) {
 	a := &app{}
-	a.errChan = make(chan error)
 
+	cfg, err := configs.MustLoad()
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Init(cfg.LogLevel, cfg.LogLevelFile, cfg.LogsDir)
+
+	s, err := store.New(cfg.DBFile)
+	if err != nil {
+		return nil, err
+	}
+
+	a.controller = controller.New(cfg, s)
 	return a, nil
 }
 
 func (a *app) Start() {
-	// run controller
-	// run go to listen a.errChan and store it into file
+	err := a.controller.DoJob()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
